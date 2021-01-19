852bb16dcbe769c4986f5637b00bc2ce4b231885
mculep <mculep@gmail.com>
2021-01-19 10:07:15 -0800

    added readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# Reviews following topics:
+
+-   callbacks
+-   closures
+-   addEventListener
+-   spread operator
+    -   for objects and for arrays
+-   destructuring syntax
+-   `&&` and `||`
+
+# Intros:
+
+-   Promises
+-   Prototypes
+-   Classes
+
+Run your python server
+
+-   `python3 -m http.server 3003`
+
+## Callback
+
+-   is an argument that is a function
+    -   .then(response => response.json())
+
+## Promises
+
+-   it's an object
+-   with a .then() function
+    -   pass it a call back function
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,34 @@
-console.log('hello world')
+console.log('hello world');
+
+
+// function processResponse(response) {
+//     return response.json
+
+
+// Show the joke on the page:
+// 1. Make the request using fetch()
+fetch('https://icanhazdadjoke.com', {
+    headers: {
+        Accept: 'application/json'
+    }
+})
+.then(response => {
+    console.log(response)
+    const theData = response.json();
+     return theData;  // returns to the next .then()
+}) // convert to JSON
+
+.then(data => { // data is plain Javascript Object
+    console.log(data);
+    return data.json;
+})
+
+.then(theJoke => {
+    console.log(theJoke);
+})
+
+.catch(err => console.log(err))
+
+// 2. Grab the .joke
+// 3. Render it to the page
+
